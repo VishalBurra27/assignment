9e538fec5dc902978d8ccc5977c60b3bfbdb0911
VishalBurra27 <vishalmathews2004@gmail.com>
2022-04-21 17:41:35 +0530

    Sixth commit

---
+++ codes/output.py
@@ -0,0 +1,15 @@
+a1=2
+b1=-1
+a2=3
+b2=-1
+c1=-8
+c2=-27
+x=int(input("Enter the value of 'a' : "))
+y=int(input("Enter the value of 'b' : "))
+
+if(a1*x+b1*y==c1 and a2*x + b2*y == c2):
+    print("Given values of 'a' and 'b' are solutions of equations 2a-b=-8 & 3a-b=-27")
+else :
+    print("Given values of 'a' and 'b' are NOT solutions of equations 2a-b=-8 & 3a-b=-27")
+    
+    
